d6865c40d3b0aa36f5ec976ef7e0114889df2567
Steve Geluso <stevegeluso@gmail.com>
2017-02-07 18:41:01 -0800

    adding shell script

---
+++ convert.sh
@@ -0,0 +1,12 @@
+echo "rushmore nl"
+python2 subjs.py Rushmore/Rushmore-NL.srt > rushmore-nl.js
+echo "rushmore eng"
+python2 subjs.py Rushmore/Rushmore-EN.srt > rushmore-en.js
+echo "primer eng"
+python2 subjs.py Primer/Primer-EN.srt > primer-en.js
+echo "life eng"
+python2 subjs.py Life-Is-Beautiful/Life-Is-Beautiful-EN.srt > life-is-beautiful-en.js
+echo "life it"
+python2 subjs.py Life-Is-Beautiful/Life-Is-Beautiful-IT.srt > life-is-beautiful-it.js
+echo "life sp"
+python2 subjs.py Life-Is-Beautiful/Life-Is-Beautiful-SP.srt > life-is-beautiful-sp.js
